217c7173b2cfecbc8118298a4b8edd6ee4a1905a
W Brian Leonard <william.leonard@oracle.com>
2016-12-01 21:14:56 -0500

    JB's upgraded JET release

---
+++ doc_root/js/appController.js
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2014, 2016, Oracle and/or its affiliates.
+ * The Universal Permissive License (UPL), Version 1.0
+ */
+/*
+ * Your application specific code will go here
+ */
+define(['ojs/ojcore', 'knockout', 'ojs/ojrouter', 'ojs/ojknockout', 'ojs/ojarraytabledatasource',
+  'ojs/ojoffcanvas'],
+  function(oj, ko) {
+     function ControllerViewModel() {
+       var self = this;
+
+      // Media queries for repsonsive layouts
+      var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
+      self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
+      var mdQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
+      self.mdScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
+
+       // Router setup
+       self.router = oj.Router.rootInstance;
+       self.router.configure({
+         'stocks': {label: 'Stocks', isDefault: true},
+         'incidents': {label: 'Incidents'},
+         'tweets': {label: 'Social'},
+         'data': {label: 'Data'}
+       });
+      oj.Router.defaults['urlAdapter'] = new oj.Router.urlParamAdapter();
+
+      // Navigation setup
+      var navData = [
+      {name: 'Stocks', id: 'stocks',
+       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'},
+      {name: 'Incidents', id: 'incidents',
+       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24'},
+      {name: 'Social', id: 'tweets',
+       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-people-icon-24'},
+      {name: 'Data', id: 'data',
+       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'}
+      ];
+      self.navDataSource = new oj.ArrayTableDataSource(navData, {idAttribute: 'id'});
+
+      // Drawer
+      // Called by nav drawer option change events so we can close drawer after selection
+      self.navChangeHandler = function (event, data) {
+       if (data.option === 'selection' && data.value !== self.router.stateId()) {
+         self.toggleDrawer();
+       }
+      }
+      // Close offcanvas on medium and larger screens
+      self.mdScreen.subscribe(function() {oj.OffcanvasUtils.close(self.drawerParams);});
+      self.drawerParams = {
+        displayMode: 'push',
+        selector: '#navDrawer',
+        content: '#pageContent'
+      };
+      // Called by navigation drawer toggle button and after selection of nav drawer item
+      self.toggleDrawer = function() {
+        return oj.OffcanvasUtils.toggle(self.drawerParams);
+      }
+
+      // Header
+      // Application Name used in Branding Area
+      self.appName = ko.observable("App Name");
+      // User Info used in Global Navigation area
+      self.userLogin = ko.observable("john.hancock@oracle.com");
+
+      // Footer
+      function footerLink(name, id, linkTarget) {
+        this.name = name;
+        this.linkId = id;
+        this.linkTarget = linkTarget;
+      }
+      self.footerLinks = ko.observableArray([
+        new footerLink('About Oracle', 'aboutOracle', 'http://www.oracle.com/us/corporate/index.html#menu-about'),
+        new footerLink('Contact Us', 'contactUs', 'http://www.oracle.com/us/corporate/contact/index.html'),
+        new footerLink('Legal Notices', 'legalNotices', 'http://www.oracle.com/us/legal/index.html'),
+        new footerLink('Terms Of Use', 'termsOfUse', 'http://www.oracle.com/us/legal/terms/index.html'),
+        new footerLink('Your Privacy Rights', 'yourPrivacyRights', 'http://www.oracle.com/us/legal/privacy/index.html')
+      ]);
+     }
+
+     return new ControllerViewModel();
+  }
+);
---
+++ doc_root/js/viewModels/incidents.js
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2014, 2016, Oracle and/or its affiliates.
+ * The Universal Permissive License (UPL), Version 1.0
+ */
+/*
+ * Your incidents ViewModel code goes here
+ */
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojchart'],
+ function(oj, ko, $) {
+  
+    function IncidentsViewModel() {
+      var self = this;
+      // Below are a subset of the ViewModel methods invoked by the ojModule binding
+      // Please reference the ojModule jsDoc for additionaly available methods.
+
+      /**
+       * Optional ViewModel method invoked when this ViewModel is about to be
+       * used for the View transition.  The application can put data fetch logic
+       * here that can return a Promise which will delay the handleAttached function
+       * call below until the Promise is resolved.
+       * @param {Object} info - An object with the following key-value pairs:
+       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
+       * @param {Function} info.valueAccessor - The binding's value accessor.
+       * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
+       * the promise is resolved
+       */
+	   
+	    /* chart data */
+        var bubbleSeries = [{name : "Series 1", items : [{x:15, y:25, z:5}, {x:25, y:30, z:12}, {x:25, y:45, z:12}]},
+                            {name : "Series 2", items : [{x:15, y:15, z:8}, {x:20, y:35, z:14}, {x:40, y:55, z:35}]},
+                            {name : "Series 3", items : [{x:10, y:10, z:8}, {x:18, y:55, z:10}, {x:40, y:50, z:18}]},
+                            {name : "Series 4", items : [{x: 8, y:20, z:6}, {x:11, y:30, z: 8}, {x:30, y:40, z:15}]}];
+
+        var bubbleGroups = ["Group A", "Group B", "Group C"];
+
+
+        this.bubbleSeriesValue = ko.observableArray(bubbleSeries);
+        this.bubbleGroupsValue = ko.observableArray(bubbleGroups);
+	   
+	   
+	   
+      self.handleActivated = function(info) {
+        // Implement if needed
+      };
+
+      /**
+       * Optional ViewModel method invoked after the View is inserted into the
+       * document DOM.  The application can put logic that requires the DOM being
+       * attached here.
+       * @param {Object} info - An object with the following key-value pairs:
+       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
+       * @param {Function} info.valueAccessor - The binding's value accessor.
+       * @param {boolean} info.fromCache - A boolean indicating whether the module was retrieved from cache.
+       */
+      self.handleAttached = function(info) {
+        // Implement if needed
+      };
+
+
+      /**
+       * Optional ViewModel method invoked after the bindings are applied on this View. 
+       * If the current View is retrieved from cache, the bindings will not be re-applied
+       * and this callback will not be invoked.
+       * @param {Object} info - An object with the following key-value pairs:
+       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
+       * @param {Function} info.valueAccessor - The binding's value accessor.
+       */
+      self.handleBindingsApplied = function(info) {
+        // Implement if needed
+      };
+
+      /*
+       * Optional ViewModel method invoked after the View is removed from the
+       * document DOM.
+       * @param {Object} info - An object with the following key-value pairs:
+       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
+       * @param {Function} info.valueAccessor - The binding's value accessor.
+       * @param {Array} info.cachedNodes - An Array containing cached nodes for the View if the cache is enabled.
+       */
+      self.handleDetached = function(info) {
+        // Implement if needed
+      };
+    }
+
+    /*
+     * Returns a constructor for the ViewModel so that the ViewModel is constrcuted
+     * each time the view is displayed.  Return an instance of the ViewModel if
+     * only one instance of the ViewModel is needed.
+     */
+    return new IncidentsViewModel();
+  }
+);
